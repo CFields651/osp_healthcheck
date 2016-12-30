9f9e29d1a6fc74b51b08376cb64e0e0b373e0332
root <root@undercloud.redhat.local>
2016-12-29 23:37:20 -0500

    updates

--- health_check.sh
+++ health_check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+tests=$1
 echo -e "This script assumes that: \n \
   1) It will be run from the undercloud as root\n \
   2) root can ssh to each overcloud controller\n \
@@ -31,9 +32,15 @@ ssh root@$controller0 "mysql -e \"show variables like 'wsrep_cluster%'\""
 ssh root@$controller0 "mysql  -e \"show status;\" | grep -E \"(wsrep_local_state_comment|wsrep_cluster_size|wsrep_ready|state_uuid|conf_id|cluster_status|wsrep_ready|wsrep_connected|local_state_comment|incoming_address|last_committed)\""
 echo " "
 echo "### rabbitmq status ###"
+#keep this command around for deeper troubleshooting
+#rabbitmqctl list_queues | awk '{ print $1,$2 }'| while read queue value; do if [ "$value" != "0" ]; then echo $queue $value;fi; don
 ssh root@$controller0 rabbitmqctl cluster_status
 echo " "
+echo "Stopped Pacemaker resources"
 ssh root@$controller0 pcs status | grep Stopped -B 2
+echo " "
+echo "Pacemaker failed actions"
+ssh root@$controller0 crm_mon  -1 | grep 'Failed Actions' -A 99
 echo " "
 echo "### ceph status ###"
 ssh root@$controller0 ceph -s 
@@ -62,9 +69,30 @@ ssh root@$controller1 'for service in nova neutron glance cinder ceph; do echo -
 echo " "
 ssh root@$controller2 'for service in nova neutron glance cinder ceph; do echo -e "\n######## controller2 $service #################"; tail -n 500 /var/log/$service/*.log | egrep "(ERROR|WARN)" | tail -3 ; done' | while read line; do filterLog "$line"; done
 echo " "
-echo -e "Run tempest.scenario.test_network_basic_ops - \nAre you in a directory where tempest tests can be run?" 
-read -p "Press Enter to continue..."
-if [ -e etc/tempest.conf ]; then  su -c "ostestr --pdb tempest.scenario.test_network_basic_ops" stack
-else echo "etc/tempest.conf not found; won't try to run test" 
+if [ "$tests" = "all" ]; then 
+  echo "Show scenario issues in service logs that are type INFO"
+  read -p "Press enter to continue..."
+  echo -e "\n#########controller 0 Port not present##########"
+  ssh root@$controller0 'grep "Port [0-9a-z].* not present in bridge" /var/log/neutron/openvswitch-agent.log' | while read line; do filterLog "$line"; done
+  echo -e "\n#########controller 1 Port not present##########"
+  ssh root@$controller1 'grep "Port [0-9a-z].* not present in bridge" /var/log/neutron/openvswitch-agent.log' | while read line; do filterLog "$line"; done
+  echo -e "\n#########controller 2 Port not present##########"
+  ssh root@$controller2 'grep "Port [0-9a-z].* not present in bridge" /var/log/neutron/openvswitch-agent.log' | while read line; do filterLog "$line"; done
+  # additional scenarios
+  # grep 'Failed to connect to libvirt' /var/log/nova/nova-compute.log; #this caused the 'Port not present error in openvswitch'
 fi
 
+if [ ! -e etc/tempest.conf ]; then 
+  echo "etc/tempest.conf not found; won't try to run tests" 
+else
+  echo -e "a) Run tempest.test_network_basic_ops "
+  echo -e "b) Run tempest.test_minimum_basic.py "
+  echo -e "q) quit"
+  read -p "What do you want to do: " answer
+
+  if [ "$answer" == "a" ]; then
+    su -c "ostestr --pdb tempest.scenario.test_network_basic_ops" stack
+  elif [ "$answer" == "b" ]; then
+    su -c "ostestr --pdb tempest.scenario.test_minimum_basic" stack
+  fi
+fi
